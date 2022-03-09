ed28977c855a3a1c181cff3c6bb24cc9948b97fd
Luca Di Molfetta <luca@pop-os.localdomain>
2022-03-09 15:26:12 +0100

    added middlewares packages like morgan helmet ecc..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,14 @@ const Auth = () => {
   return <h1>Porca Madonna</h1>;
 };
 
+const NotFound = () => {
+  return <h1>Not found</h1>;
+};
+
+const Test2 = () => {
+  return <h1>Stupidooooo</h1>;
+};
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -21,7 +29,8 @@ function App() {
         <Layout>
           <Routes>
             <Route exact path="/" element={<Test />} />
-            <Route path="/auth" element={<Auth />}></Route>
+            <Route path="/auth" element={<Auth />} />
+            <Route path="*" element={<Test2 />} />
           </Routes>
         </Layout>
       </Router>
--- server/app/app.js
+++ server/app/app.js
@@ -3,19 +3,35 @@ const express = require("express");
 const path = require("path");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const morgan = require("morgan");
+const cors = require("cors");
+const { StatusCodes } = require("http-status-codes");
 require("dotenv").config();
 
 //app declaration
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
-  console.log("porco dio");
+  app.use(morgan);
 }
+
+app.use(helmet());
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(cors());
+
+//serving static files
 app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
+});
+
+//not found
+app.all("*", (req, res, next) => {
+  res.redirect("/not-found");
 });
 
 module.exports = app;
