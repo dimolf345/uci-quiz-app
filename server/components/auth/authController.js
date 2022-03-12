0a73dba1ff8a33fb040bcccce0107012ad03acf6
Luca <dimolfetta.ta@gmail.com>
2022-03-12 23:50:07 +0100

    added error handling for casterror and duplicate fields

--- server/components/auth/authController.js
+++ server/components/auth/authController.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const User = require("../user/userModel");
+const catchAsync = require("../error/catchAsync");
 
 const generateAccessToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
@@ -18,35 +19,33 @@ const setTokenCookie = (response, token) => {
   response.cookie("jwt", token, cookieOptions);
 };
 
-exports.signin = async (req, res, next) => {
-  try {
-    const { search, password } = req.body;
-    const user = await User.findOne({
-      $or: [{ username: search }, { email: search }],
-    }).select("+hashed_password +role");
-    if (!user) {
-      res.status(StatusCodes.NOT_FOUND).json({
-        status: "fail",
-        message: "User not found",
-      });
-    }
-    if (user.checkPassword(password)) {
-      console.log(user);
-      const token = generateAccessToken(user._id);
-      user.last_access = Date.now();
-      await user.save({ validateBeforeSave: false });
-      setTokenCookie(res, token);
-      res.status(StatusCodes.OK).json({
-        status: "success",
-        token,
-        user: {
-          username: user.username,
-          last_access: user.last_access,
-          role: user.role,
-        },
-      });
-    }
-  } catch (error) {
-    console.log(error);
+exports.signin = catchAsync(async (req, res, next) => {
+  const { search, password } = req.body;
+  const user = await User.findOne({
+    $or: [{ username: search }, { email: search }],
+  }).select("+hashed_password +role");
+  if (!user) {
+    res.status(StatusCodes.NOT_FOUND).json({
+      status: "fail",
+      message: "User not found",
+    });
   }
-};
+  if (user.checkPassword(password)) {
+    const token = generateAccessToken(user._id);
+    user.last_access = Date.now();
+    await user.save({ validateBeforeSave: false });
+    setTokenCookie(res, token);
+    res.status(StatusCodes.OK).json({
+      status: "success",
+      token,
+      user: {
+        username: user.username,
+        last_access: user.last_access,
+        role: user.role,
+      },
+    });
+  } else {
+    console.log("Calling next function...");
+    next();
+  }
+});
